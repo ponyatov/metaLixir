582f2a6eef25be7b6357fea641c985b6bec5ff46
Dmitry Ponyatov <dponyatov@gmail.com>
2020-10-31 15:54:46 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+#  `metaLixir`
+## `metaL` circular implementation in Elixir/Erlang
+
+* automatic code generation
+* homoiconic (meta)language for source code transformations
+
+(c) Dmitry Ponyatov <<dponyatov@gmail.com>> 2020 MIT
+
+github: https://github.com/ponyatov/metaLixir
--- exs.exs
+++ exs.exs
@@ -1,8 +1,37 @@
-dir = "."
+app     = "metaLixir"
+title   = "`metaL` circular implementation in Elixir/Erlang"
+author  = "Dmitry Ponyatov"
+email   = "dponyatov@gmail.com"
+license = "MIT"
+github  = "https://github.com/ponyatov/#{app}"
 
-File.write("#{dir}/mix.exs","""
+dir = "."
+lib = "#{dir}/lib"
+
+File.write("#{dir}/.formatter.exs", """
+[ inputs: [
+    "{config,lib,test}/**/*.{ex,exs}",
+    "{mix,.formatter}.exs"
+] ]
 """)
 
-File.write("#{dir}/.gitignore","""
+File.write("#{dir}/mix.exs", """
+""")
+
+File.write("#{dir}/.gitignore", """
 /.elixir_ls/
+""")
+
+File.mkdir(lib)
+
+File.write("#{dir}/README.md", """
+#  `#{app}`
+## #{title}
+
+* automatic code generation
+* homoiconic (meta)language for source code transformations
+
+(c) #{author} <<#{email}>> 2020 #{license}
+
+github: #{github}
 """)
